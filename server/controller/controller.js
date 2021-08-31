f0d758a1a0a868e27ff57beeb75ddb7e94fdf459
andika jaya wiguna <andikajayawiguna@gmail.com>
2021-08-31 23:08:03 +0800

    add api controller and update router

---
+++ server/controller/controller.js
@@ -0,0 +1,20 @@
+var userDb = require('../model/model');
+
+//c create and save new user
+exports.create = (req, res) => {
+
+}
+
+// retrieve and return all users/retrive and return a single user
+exports.find = (req, res) => {
+
+}
+
+// Update a new identified by user id 
+exports.update = (req, res) => {
+
+}
+// Delete a user
+exports.delete = (req, red) => {
+
+}
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 
 const services = require('../services/render');
+const controller = require('../controller/controller');
 
 /**
  * @description Root Route
@@ -20,5 +21,11 @@ route.get('/add_user', services.add_user);
  * @method GET /update_user
  */
 route.get('/update_user', services.update_user);
+
+// API
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
 
 module.exports = route;
