17af1926dbe0601fddfbde55ea3a73a5863d01ca
andika jaya wiguna <andikajayawiguna@gmail.com>
2021-09-05 23:36:14 +0800

    create update user function using ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
 $("#add_user").submit(function (event) {
     alert("Data Inserted Successfully");
 })
+
+$("#update_user").submit(function (event) {
+    event.preventDefault();
+    var unindexed_array = $(this).serializeArray();
+    var data = {};
+
+    $.map(unindexed_array, function (n, i) {
+        data[n['name']] = n['value']
+    })
+
+    console.log(data);
+
+    var request = {
+        "url": `http://localhost:3000/api/users/${data.id}`,
+        "method": "PUT",
+        "data": data
+    }
+    $.ajax(request).done(function (response) {
+        alert('Data updated!');
+    });
+})
