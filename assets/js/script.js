7f4203aa15f0d2188af57377a7fa82a588754240
andika jaya wiguna <andikajayawiguna@gmail.com>
2021-09-05 23:49:36 +0800

    create delete user function

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,22 @@ $("#update_user").submit(function (event) {
         alert('Data updated!');
     });
 })
+
+if (window.location.pathname == "/") {
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function () {
+        var id = $(this).attr("data-id")
+        var request = {
+            "url": `http://localhost:3000/api/users/${id}`,
+            "method": "DELETE"
+        }
+
+        if (confirm("Delete this user ?")) {
+            $.ajax(request).done(function (response) {
+                alert('User deleted');
+                location.reload();
+            })
+        }
+    })
+
+}
